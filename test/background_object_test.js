4710649a2f3b393bce031ca9781b78982a03214c
Angela Lindow <angelalindow@gmail.com>
2016-10-12 15:15:03 -0600

    Pull models out to their own files

---
+++ test/background_object_test.js
@@ -0,0 +1,34 @@
+// const chai = require('chai');
+// const assert = chai.assert;
+// const BackgroundObject = require('../lib/background_object')
+// const Game = require('../lib/game')
+//
+// describe('BackgroundObject', function(){
+//   context('with given attributes', function(){
+//     var backgroundObject = new BackgroundObject({source: 'backgroundObject.png', x: 50, y: 300, width: 150, height: 250});
+//     it('should be instantiated', function(){
+//       assert.instanceOf(backgroundObject, BackgroundObject);
+//     });
+//
+//     it('should have an img', function(){
+//       assert.instanceOf(backgroundObject.img), Image);
+//       assert.equal(backgroundObject.img.src, 'backgroundObject.png');
+//     });
+//
+//     it('should have an x coordinate', function(){
+//       assert.equal(backgroundObject.x, 50);
+//     });
+//
+//     it('should have a y coordinate', function(){
+//       assert.equal(backgroundObject.y, 300);
+//     });
+//
+//     it('should have a width', function(){
+//       assert.equal(backgroundObject.width, 150);
+//     });
+//
+//     it('should have a height', function(){
+//       assert.equal(backgroundObject.height, 250);
+//     });
+//   });
+// });
--- test/obstacle_test.js
+++ test/obstacle_test.js
@@ -4,13 +4,14 @@ const Obstacle = require('../lib/obstacle')
 
 describe('Obstacle', function(){
   context('with given attributes', function(){
-    var obstacle = new Obstacle('obstacle.png', 50, 300, 150, 250);
+    var obstacle = new Obstacle('obstacle.png', 50, 300, 150, 250, 20);
     it('should be instantiated', function(){
       assert.instanceOf(obstacle, Obstacle);
     });
 
     it('should have an img', function(){
-      assert.equal(obstacle.img, 'obstacle.png');
+      assert.instanceOf(obstacle, Obstacle);
+      assert.equal(obstacle.img.src, 'obstacle.png');
     });
 
     it('should have an x coordinate', function(){
@@ -27,6 +28,10 @@ describe('Obstacle', function(){
 
     it('should have a height', function(){
       assert.equal(obstacle.height, 250);
+    });
+
+    it('should have a speed', function(){
+      assert.equal(obstacle.speed, 20);
     });
   });
 });
