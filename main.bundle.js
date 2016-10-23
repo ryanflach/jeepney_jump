26f226c9d837fb697f5193a9329bcfe003155457
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-23 08:56:59 -0600

    Remove arrow syntax from service worker

--- main.bundle.js
+++ main.bundle.js
@@ -87,10 +87,10 @@
 
 	const registerServiceWorker = () => {
 	  if ('serviceWorker' in navigator) {
-	    navigator.serviceWorker.register('/jeepney_jump/service_worker.js').then(reg => {
+	    navigator.serviceWorker.register('/jeepney_jump/service_worker.js').then(function (reg) {
 	      // registration worked
 	      console.log('Registration succeeded. Scope is ' + reg.scope);
-	    }).catch(error => {
+	    }).catch(function (error) {
 	      // registration failed
 	      console.log('Registration failed with ' + error);
 	    });
